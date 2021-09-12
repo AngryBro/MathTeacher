da4070b3e7b99b0d10e31eefc012fbc7318b6cea
AngryBro <67253990+AngryBro@users.noreply.github.com>
2021-09-12 13:28:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function OpenList(id) {
         list.hidden = true;
     }
 }
+
+function CloseLists(id1, id2, id3) {
+    var lists = [3];
+    lists[0] = document.getElementById(id1);
+    lists[1] = document.getElementById(id2);
+    lists[2] = document.getElementById(id3);
+    for(var i = 0; i<3; i++) {
+        lists[i].hidden = true;
+    }
+}
